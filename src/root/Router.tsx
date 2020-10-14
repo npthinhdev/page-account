eaa4c599b38117ddbf99abf3c02f2f95487da293
Thinh Nguyen Phuoc <npthinh1996@gmail.com>
2020-10-14 18:01:33 +0700

    redirect / to /account

--- src/root/Router.tsx
+++ src/root/Router.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-dom';
 import { Account } from '../pages';
 
 class AppRouter extends React.Component {
@@ -7,6 +7,9 @@ class AppRouter extends React.Component {
     return (
       <Router>
         <Switch>
+          <Route exact path="/">
+            <Redirect to="/account" />
+          </Route>
           <Route path="/account" component={Account} />
         </Switch>
       </Router>
