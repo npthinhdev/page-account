6a1c72c746d91df593f87b6c6ea53fafb7764c77
Thinh Nguyen Phuoc <npthinh1996@gmail.com>
2020-10-15 07:19:13 +0700

    using render.tsx

--- src/pages/Account/index.tsx
+++ src/pages/Account/index.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import AccountRender from './render';
 
 class Account extends React.Component {
   render() {
     return (
-      <div>Test page Account</div>
+      <AccountRender />
     )
   }
 }
---
+++ src/pages/Account/render.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class AccountRender extends React.Component {
+  render() {
+    return (
+      <div>Using render.tsx</div>
+    )
+  }
+}
+
+export default AccountRender
